d01db91c141fe5fde4321eb037639d08f902fdce
ethantran <ethantran0815@gmail.com>
2024-11-25 15:34:07 -0500

    One jump and pulling arms in.

--- simulation.py
+++ simulation.py
@@ -1,10 +1,12 @@
 from vpython import *
 import skater
+import math
 #
 # Assumed Body Constants
 #
 torso_radius = 0.15
 torso_length = 0.50
+torso_mass = 50
 
 thigh_length =.4
 calf_length =.4
@@ -12,8 +14,9 @@ thigh_radius = 0.085
 calf_radius = .0607
 thigh_spring_constant =7000
 calf_spring_constant =7000
+leg_mass= 10
 
-leg_spring_constant = 14000
+leg_spring_constant = 20000
 
 #Arm parameters
 brachium_length = .23*1.5
@@ -22,6 +25,9 @@ brachium_radius = .04
 forearm_length = brachium_length
 forearm_radius = .03
 
+arm_length = forearm_length + brachium_length
+arm_mass = 5
+
 leg_length = thigh_length+calf_length
 
 body_mass = 80
@@ -29,7 +35,7 @@ g = 9.8
 
 #
 scene = canvas(title= "Figure Skater Simulation", background = color.white)
-figure = skater.Skater(torso_radius=torso_radius, torso_length=torso_length, thigh_length= thigh_length, thigh_radius=thigh_radius, calf_length= calf_length, calf_radius=calf_radius, leg_spring_constant=leg_spring_constant,forearm_length=forearm_length, brachium_length=brachium_length, brachium_radius=brachium_radius, forearm_radius= forearm_radius)
+figure = skater.Skater(torso_radius=torso_radius, torso_length=torso_length, torso_mass= torso_mass, thigh_length = thigh_length, thigh_radius=thigh_radius, calf_length= calf_length, calf_radius=calf_radius, leg_mass = leg_mass, leg_spring_constant=leg_spring_constant,forearm_length=forearm_length, brachium_length=brachium_length, brachium_radius=brachium_radius, forearm_radius= forearm_radius, arm_mass= arm_mass)
 scene.camera.pos = vector(-scene.camera.pos.z, scene.camera.pos.y+1, scene.camera.pos.x)
 scene.camera.axis = (figure.torso.pos - scene.camera.pos)
 scene.autoscale = False
@@ -54,15 +60,19 @@ ground = box(pos = vector(0,-0.1/2,0), length = 2, width = 100, height = 0.1, co
 
 initial_compression  = vector(0,.3,0)
 
-horizontal_velocity = 7
+horizontal_velocity = -7
 figure.mom = vector(0,0,horizontal_velocity*body_mass)
 figure.squat(initial_compression)
 figure.spring_force = vector(0,0,0)
 
 dt = 0.005
 t = 0.0
+figure.angmom = vector(0, 2*math.pi*4*body_mass, 0)
+k = 0
+controlled_rate = 50
+
 while(True):
-    rate(50)
+    rate(controlled_rate)
     t += dt
 
     #this assumes massless legs
@@ -71,6 +81,10 @@ while(True):
     figure.spring_force = -leg_spring_constant*(figure.left_leg.axis-vector(0,leg_length,0))
     netforce = gravityforce + figure.spring_force
     # netforce = vector(0,0,0)
+
+    angle = mag(figure.angmom / body_mass) * dt
+    figure.rotate_cm(angle)
+
     figure.mom += netforce*dt
     pos_delta = (figure.mom/body_mass)*dt
     figure.move_body_y(pos_delta, range(4))
@@ -79,33 +93,28 @@ while(True):
     if figure.left_leg.axis.y >= leg_length:
         if figure.left_leg.pos.y + pos_delta.y <= 0:
             figure.stretch(pos_delta.y)
+            k = 1
+            print(t)
         else:
             figure.move_body_y(pos_delta, (4,5))
     else:
         figure.stretch(pos_delta.y)
+        if(pos_delta.y > 0 and k ==1):
+            print(t)
+            break
+
+    if figure.left_leg.pos.y > ground.pos.y:
+        figure.arms_in(arm_length*dt/.1)
+    if k==1:
+        figure.arms_out(-arm_length*dt/.1)
 
 
+while(True):
+    rate(controlled_rate)
+    figure.arms_out(-arm_length*dt/.1)
+    figure.move_body_z(pos_delta, range(6))
 
-
-
-
-
-
-
-
-    # if mag(skater.left_leg.axis) < leg_length:
-    #     skater.stretch(pos_delta)
-    #     if skater.left_leg.axis + pos_delta > leg_length:
-    # else:
-    #     if skater.left_leg.pos.y + pos_delta.y <0:
-    #         skater.left_leg.pos.y = 0
-    #         skater.right_leg.pos.y= 0
-    #         skater.stretch(pos_delta)
-    #     else:
-    #         skater.move_body(pos_delta, (4, 5))
-
-    if(t >10):
-        break
+    t += dt
 
 
 
--- skater.py
+++ skater.py
@@ -2,7 +2,7 @@ from vpython import *
 import math
 
 class Skater:
-    def __init__(self, torso_radius, torso_length, thigh_length, thigh_radius, calf_length, calf_radius, leg_spring_constant, brachium_length, brachium_radius, forearm_length, forearm_radius):
+    def __init__(self, torso_radius, torso_length, torso_mass, thigh_length, thigh_radius, calf_length, calf_radius, leg_spring_constant, leg_mass,brachium_length, brachium_radius, forearm_length, forearm_radius, arm_mass):
 
         # self.forearm_radius = forearm_radius
         # self.forearm_length = forearm_length
@@ -13,32 +13,34 @@ class Skater:
         # self.thigh_radius = thigh_radius
         # self.thigh_length = thigh_length
         # self.torso_length = torso_length
+
         self.leg_length = calf_length + thigh_length
         length = vector(0, self.leg_length, 0)
-        self.left_leg = cylinder(pos=vector(-torso_radius / 2, 0, 0), axis=length, radius=thigh_radius,
+        self.left_leg = cylinder(pos=vector(-torso_radius / 2, 0, 0), axis=length, radius=thigh_radius, mass = leg_mass,
                                  color=color.green,
                                  ks=leg_spring_constant)
-        self.right_leg = cylinder(pos=vector(torso_radius / 2, 0, 0), axis=length, radius=thigh_radius,
+        self.right_leg = cylinder(pos=vector(torso_radius / 2, 0, 0), axis=length, radius=thigh_radius, mass = leg_mass,
                                   color=color.green,
                                   ks=leg_spring_constant)
 
         self.head_radius = .1
         self.head_length = .23
+        self.head_mass = 4
 
         # Upper body
-        self.torso = cylinder(pos=self.left_leg.axis, axis=vector(0, torso_length, 0), radius=torso_radius, color=color.blue)
+        self.torso = cylinder(pos=self.left_leg.axis, axis=vector(0, torso_length, 0), radius=torso_radius, mass= torso_mass,color=color.blue)
         self.head = cylinder(pos=self.torso.pos+self.torso.axis, axis=vector(0, self.head_length, 0),
-                               radius=self.head_radius, color=color.orange)
+                               radius=self.head_radius, color=color.orange, mass = self.head_mass)
 
         # arms
         # self.left_arm = Arm(pos = self.head.pos + vector(-torso_radius, -brachium_radius, 0), brachium_length= brachium_length, brachium_radius=brachium_radius, forearm_length=forearm_length)
         # self.right_arm = Arm(pos = self.head.pos + vector(torso_radius, -brachium_radius, 0), brachium_length= brachium_length, brachium_radius=brachium_radius, forearm_length=forearm_length)
 
-        self.left_arm = cylinder(pos=self.head.pos + vector(-torso_radius, -brachium_radius, 0), axis=(brachium_length + forearm_length) * vector(0, -1, 0),
-                            radius=brachium_radius, color=color.red)
-        self.right_arm = cylinder(pos = self.head.pos + vector(torso_radius, -brachium_radius, 0), axis=(brachium_length + forearm_length) * vector(0, -1, 0),
-                            radius=brachium_radius, color=color.yellow)
-
+        self.left_arm = cylinder(pos=self.head.pos + vector(-torso_radius, -brachium_radius, 0), axis=(brachium_length + forearm_length) * vector(-1, 0, 0),
+                            radius=brachium_radius, mass = arm_mass,color=color.red)
+        self.right_arm = cylinder(pos = self.head.pos + vector(torso_radius, -brachium_radius, 0), axis=(brachium_length + forearm_length) * vector(1, 0, 0),
+                            radius=brachium_radius, mass = arm_mass, color=color.yellow)
+        self.arm_length = brachium_length + forearm_length
         # self.left_brachium = cylinder(pos=, axis = brachium_length*vector(),radius= brachium_radius, color = color.magenta)
         # self.right_brachium = cylinder(pos=self.head.pos + vector(torso_radius, -brachium_radius, 0), radius= brachium_radius, color = color.red)
 
@@ -67,9 +69,25 @@ class Skater:
         self.body_components[4].axis.y += pos_delta
         self.body_components[5].axis.y += pos_delta
 
+    def rotate_cm(self, angle):
+        for component in self.body_components:
+            component.rotate(axis = self.torso.axis, angle = angle, origin = self.torso.pos)
 
+    def moi(self):
+        moi = 0
+        for component in self.body_components:
+            moi += component.mass * (component.pos+component.axis/2 - self.torso.pos)
+        return moi
 
+    def arms_in(self, length):
+        if mag(self.body_components[2].axis)  > 0:
+            self.body_components[2].axis -= length * norm(self.body_components[2].axis)
+            self.body_components[3].axis -= length * norm(self.body_components[3].axis)
 
+    def arms_out(self, length):
+        if mag(self.body_components[2].axis) < self.arm_length:
+            self.body_components[2].axis -= length * norm(self.body_components[2].axis)
+            self.body_components[3].axis -= length * norm(self.body_components[3].axis)
 
 # class Arm:
 #     def __init__(self, pos, brachium_length, brachium_radius, forearm_length):
